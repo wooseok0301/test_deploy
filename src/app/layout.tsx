5d43e5eae0d613fa7f424f4ec696ff8df2076385
review22 <sws010301@naver.com>
2025-10-23 18:02:32 +0900

    카톡수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,21 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Clix',
   description: '프로젝트 공유 플랫폼',
+  openGraph: {
+    title: 'Clix: 프로젝트 공유 플랫폼',
+    description: '다양한 프로젝트를 공유하고 영감을 얻으세요!', 
+    url: 'https://jbuis.kr', 
+    siteName: 'Clix',
+    images: [
+      {
+        url: 'https://ifh.cc/v-mDc2Tz', 
+        width: 800, 
+        height: 400, 
+        alt: 'Clix 프로젝트 미리보기', 
+      },
+    ],
+    type: 'website', 
+  },
 }
 
 export default function RootLayout({
